2a46874b0ca49b445d95c9e5239905206cd379e8
xinsheng.zxs <xinsheng.zxs@alibaba-inc.com>
2020-10-31 13:30:16 +0800

    start succ

---
+++ src/main/java/own/star/scatter/controller/WebApplication.java
@@ -0,0 +1,20 @@
+package own.star.scatter.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+    private static final Logger logger = LoggerFactory.getLogger(WebApplication.class);
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(WebApplication.class, args);
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            throw e;
+        }
+    }
+
+}
